9abc7466a3e298a1396d1aabbaaadc6f475d17d0
Pedro Ferraz <c.pedroferraz@outlook.com>
2021-09-21 06:49:56 -0300

    06 - Uma resposta padrão 1:00

---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,16 @@
+﻿namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void Configure()
+        {
+
+        }
+
+        public void LivrosParaLer()
+        {
+            var _repo = new LivroRepositorioCSV();
+            _repo.ParaLer.ToString();
+        }
+    }
+}
